3e182c4805e24d21304b0d96079072fb6cb6e6ca
kolniy <kolaniyi3@gmail.com>
2021-01-22 14:38:46 +0100

    added password update route

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -123,4 +123,40 @@ async (req, res) => {
     }
 })
 
+router.put('/', 
+    body('password', 'Please enter a password with 6 or more characters').isLength({ min: 6}),
+    auth, 
+    async (req, res) => {
+
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+
+    const userId = req.user.id
+    const newPassword = req.body.password
+
+    try {
+    let user = await User.findById(userId)
+    if(!user){
+        return res.status(404).json({ errrors : [{msg: "user not found"}] })
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const password = await bcrypt.hash(newPassword, salt)
+
+    user.password = password
+    user.save()
+    res.json(user)
+
+    } catch (error) {
+        res.status(500).json({
+            errors: error
+        })
+        console.error(error)
+    }
+})
+
 export default router
